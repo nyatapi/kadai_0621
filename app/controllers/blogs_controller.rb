bec81bcc243e155f3108aae34b8b8258fb506707
nao machida <xo224324xo@icloud.com>
2021-06-18 14:19:51 +0900

    ブログ編集機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -18,7 +18,19 @@ class BlogsController < ApplicationController
 
   def show
     @blog = Blog.find(params[:id])
+  end
 
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
   end
 
   private
